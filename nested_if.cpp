8597771c6e23eb2cdddea843e6b7194f39fd1583
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-14 11:24:25 +0800

    Membuat fungsi nested if dimana kita akan mengecek apakah nilai rata-rata yang dihitung lebih besar atau sama dengan 60 atau tidak.

--- nested_if.cpp
+++ nested_if.cpp
@@ -5,3 +5,16 @@ using namespace std;
 float Rata_rata(float a, float b) {
     return (a + b) / 2;
 }
+
+//fungsi ini adalah contoh dari penggunaan nested if, dimana kita akan mengecek apakah nilai rata-rata yang dihitung lebih besar atau sama dengan 60 atau tidak, jika lebih besar atau sama dengan 60 maka kita akan mengecek lagi apakah nilai rata-rata tersebut lebih besar dari 80 atau tidak, jika lebih besar dari 80 maka kita akan mengembalikan string "Lulus Cumlaude", jika tidak maka kita akan mengembalikan string "Lulus Baik", jika nilai rata-rata kurang dari 60 maka kita akan mengembalikan string "Gagal".
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        if (r > 80){
+            return "Lulus Cumlaude";
+        }else{
+            return "Lulus Baik";
+        }
+    else
+        return "Gagal";
+}
