6f064b25930ca372e9668f04db3c680b8dab2e72
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-14 11:19:22 +0800

    fungsi ini adalah contoh dari penggunaan if else, dimana kita akan mengecek apakah nilai rata-rata yang dihitung lebih besar atau sama dengan 60 atau tidak,

--- else_if.cpp
+++ else_if.cpp
@@ -5,3 +5,14 @@ using namespace std;
 float Rata_rata(float a, float b) {
     return (a + b) / 2;
 }
+
+//fungsi ini adalah contoh dari penggunaan if else, dimana kita akan mengecek apakah nilai rata-rata yang dihitung lebih besar atau sama dengan 60 atau tidak, jika lebih besar atau sama dengan 60 maka kita akan mengembalikan string "Lolos", jika tidak maka kita akan mengembalikan string "Gagal".
+string Status_lulus(float r)
+{
+    if (r >= 60){
+        return "Lolos";
+    }
+    else{
+        return "Gagal";
+    }
+}
